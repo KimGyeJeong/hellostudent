042823f2f08419b04264e7c5e691696be28bc018
KimGyeJeong <pmwkdo@gmail.com>
2024-03-21 00:31:44 +0900

    success to connect mariaDB

--- server/src/models/testConnectMaria.js
+++ server/src/models/testConnectMaria.js
@@ -1,16 +1,20 @@
-const mariadb = require('mysql');
+const mariadb = require('mariadb');
 const dbConfig = require('../config/mariaConfig');
 
 class TestConnectMaria{
-    constructor() {
-        
-    }
+    // constructor() {
+    //    
+    // }
     
     async mariaConnection() {
+        // const mariadbPool = mariadb.createPool(dbConfig);
+        
         const mariadbPool = mariadb.createPool(dbConfig);
-        const conn = await mariadbPool.getConnection();
-        let sql = "select sysdate() from dual";
+        
+        let sql = "select sysdate() as currentdate from dual";
+        let conn;
         try {
+            conn = await mariadbPool.getConnection();
             let result = await conn.query(sql);
             console.log(result);
             return result;
@@ -18,7 +22,7 @@ class TestConnectMaria{
         } catch (err) {
             console.log(err);
         } finally {
-            if (conn) await conn.release();
+            if (conn) await conn.end();
         }
     }
 }
